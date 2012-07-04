f6eec141397f181a98e1480506c076086f6ea4e7
Hyeongjin, Kim <1981_khj@naver.com>
2012-07-03 21:46:00 -0400

    change

--- public/javascripts/jindo.scratch.js
+++ public/javascripts/jindo.scratch.js
@@ -1,98 +1,4 @@
-else {
-			//Mouse Event
-			this._wfMouseDown = jindo.$Fn(this._onMouseDown, this);
-			this._wfMouseMove = jindo.$Fn(this._onMouseMove, this);
-			this._wfMouseUp = jindo.$Fn(this._onMouseUp, this);
-
-			this._wfMouseDown.attach(this._elCanvas, "mousedown");
-			this._wfMouseMove.attach(this._elCanvas, "mousemove");
-			this._wfMouseUp.attach(this._elCanvas, "mouseup");
-		}
-	},
-	_hasTouch : function() {
-		console.log("_hasTouch");
-		return !!('ontouchstart' in window);
-	},
-	_setBackGroundImage : function(sImagePath) {
-		console.log("_setBackGroundImage");
-		this._welScratchPad.css({
-			backgroundImage : 'url(' + sImagePath + ')'
-		});
-	},
-	_createImage : function(){
-		this._welCanvas.attr({
-			"width" : this._nWidth,
-			"height" : this._nHeight			
-		});
-		
-		this._welScratchPad.append(this._welCanvas);
-	},
-	_drawImage : function() {
-		console.log("_drawImage");
-		this._oCtx.fillStyle = this._sColor;
-		this._oCtx.beginPath();
-		this._oCtx.rect(0, 0, this._nWidth, this._nHeight);
-		this._oCtx.fill();
-	},
-	_onTouchStart : function(oEvent) {
-		console.log("_onTouchStart");
-		oEvent.stop($Event.CANCEL_ALL);
-		this._bScratch = true;
-		this._onScratchDown(oEvent.$value().touches[0]);		
-		this._overScratchPercentage(this._getScratchPercentage());
-	},
-	_onTouchMove : function(oEvent) {
-		console.log("_onTouchMove");
-		if(this._bScratch){
-			this._onScratchMove(oEvent.$value().touches[0]);
-			this._overScratchPercentage(this._getScratchPercentage());
-		}
-	},
-	_onTouchEnd : function(oEvent) {
-		console.log("_onTouchEnd");
-		if(this._bScratch){
-			this._onScratchUp();
-			this._bScratch = false;
-			this._overScratchPercentage(this._getScratchPercentage());
-		}
-	},
-	_onMouseDown : function(oEvent) {
-		//_bScratch		
-		console.log("_onMouseDown");
-		//oEvent.stop($Event.CANCEL_DEFAULT);
-		oEvent.stop($Event.CANCEL_ALL);
-		this._bScratch = true;
-		this._onScratchDown(oEvent.pos());		
-		this._overScratchPercentage(this._getScratchPercentage());
-	},
-	_onMouseMove : function(oEvent) {
-		console.log("_onMouseMove");
-		if(this._bScratch){
-			this._onScratchMove(oEvent.pos());
-			this._overScratchPercentage(this._getScratchPercentage());
-		}
-	},
-	_onMouseUp : function(oEvent) {
-		console.log("_onMouseUp");
-		if(this._bScratch){
-			this._onScratchUp();
-			this._bScratch = false;
-			this._overScratchPercentage(this._getScratchPercentage());
-		}
-	},
-	_onScratchDown : function(oEvent){
-		console.log("_onScratchDown");
-		var oPos = this._getScratchPosition(oEvent.pageX, oEvent.pageY);
-		console.log("oPos.pageX: " + oPos.pageX + "oPos.pageY" + oPos.pageY);
-		
-		this._oCtx.globalCompositeOperation = 'destination-out';
-		this._oCtx.lineJoin = "round";
-		this._oCtx.lineCap = "round";
-		this._oCtx.strokeStyle = this._sColor;
-		this._oCtx.lineWidth = this._nCursorSize;
-
-		this._oCtx.beginPath();
-		this._oCtx.arc(oPos.pageX, oPos.pageY, this._nCursorSize / 2, 0, Math.PI * 2, true);
+Pos.pageX, oPos.pageY, this._nCursorSize / 2, 0, Math.PI * 2, true);
 		this._oCtx.closePath();
 		this._oCtx.fill();
 
@@ -121,6 +27,15 @@ else {
 		}
 		
 		return (nHits / this._nPixels) * 100;
+	},
+	_getEventPosition : function(oEvent){
+		if (oEvent.targetTouches){
+			oEvent = oEvent.targetTouches[0];
+		}
+			
+        if (oEvent.pageX != null && oEvent.pageY != null){
+        	return {pageX: oEvent.pageX, pageY: oEvent.pageY};
+        } 
 	},
 	_getScratchPosition : function(oPositionX, oPositionY){
 		var htPosition = {
